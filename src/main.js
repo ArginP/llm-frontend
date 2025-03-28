159f1cf9a7b16f72b02057dd6883400fd74e9648
ArginP <n7toman@gmail.com>
2025-03-28 13:30:56 +0300

    add main page markup, add styles

--- src/main.js
+++ src/main.js
@@ -1,24 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+import logoSrc from './assets/logo.png';
+import profileImgSrc from './assets/profile.png';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const logoPage = document.getElementById('logo');
+const profileImgPage = document.getElementById('profileImg');
 
-setupCounter(document.querySelector('#counter'))
+logoPage.src = logoSrc;
+profileImgPage.src = profileImgSrc;
