fd4f3f00d1ff6fdb5f82bfb070895e71d1536cff
ArginP <n7toman@gmail.com>
2025-03-29 15:30:24 +0300

    add scripts for messaging responsive UI

--- src/main.js
+++ src/main.js
@@ -6,5 +6,50 @@ import profileImgSrc from './assets/profile.png';
 const logoPage = document.getElementById('logo');
 const profileImgPage = document.getElementById('profileImg');
 
+const body = document.getElementById('body');
+const dialog = document.getElementById('dialog');
+const dialogWrapper = dialog.parentNode
+const inputField = document.getElementById('inputField');
+const inputBtn = document.getElementById('inputBtn');
+
 logoPage.src = logoSrc;
 profileImgPage.src = profileImgSrc;
+
+const userMessageHtml = (inputValue) => {
+    return `
+    <div class="message user-message">
+        <span>${inputValue}</span>
+    </div>
+    `
+}
+
+const assistantMessageHtml = (inputValue) => {
+    return `
+    <div class="message assistant-message">
+        <span>${inputValue}</span>
+    </div>
+    `
+}
+
+inputBtn.addEventListener('click', event => {
+    event.preventDefault();
+
+    // Удержание скролла чата в нижнем положении:
+    const isScrolledToBottom = dialogWrapper.scrollHeight - dialogWrapper.clientHeight <= dialogWrapper.scrollTop + 1
+
+    const newMessageNode = document.createElement('span');
+    newMessageNode.innerHTML = userMessageHtml(inputField.value);
+
+    dialog.appendChild(newMessageNode);
+
+    const newReplyNode = document.createElement('span');
+    newReplyNode.innerHTML = assistantMessageHtml(`Ищу ответ на ${inputField.value}`);
+
+    dialog.appendChild(newReplyNode);
+
+    body.classList.remove('initial')
+
+    if (isScrolledToBottom) { // Только если чат уже в нижнем положении
+        dialogWrapper.scrollTop = dialogWrapper.scrollHeight - dialogWrapper.clientHeight
+    }
+})
